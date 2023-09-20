11947a69d1247bc726c9bc6bce179fbb7edf66db
cogicgoran <cogicgoran99@gmail.com>
2023-09-20 13:20:04 +0200

    List locations feature. Add flights feature.

--- playground.c
+++ playground.c
@@ -12,11 +12,14 @@ Add flights.
 #define TRUE 1
 #define FALSE 0
 
-void create_location(char *start);
-// get_location(char *start);
-// create_flight(char *start, char *destination);
-
 void add_location_flow();
+int location_exists(char *location);
+void create_location(char *start);
+void list_locations();
+void add_flight();
+void create_flight(char *origin, char *destination);
+void select_location(char *location);
+
 char *to_upper_case_string(char *str);
 
 int main()
@@ -27,21 +30,21 @@ int main()
         // read cli input character
         printf("\n1: Add location\n");
         printf("2: Add flight\n");
-        printf("3: Add flight\n");
+        printf("3: List locations\n");
         printf("9: Exit\n");
         printf("Choose action: ");
         input_char = getchar();
+        // printf("---------------");
         switch (input_char)
         {
         case '1':
             add_location_flow();
-            exit(0);
             break;
         case '2':
-
+            add_flight();
             break;
         case '3':
-            // list locations
+            list_locations();
             break;
         case '9':
             // Exit program
@@ -54,9 +57,55 @@ int main()
     }
 }
 
-int location_exists(char *location)
+void select_location(char *location)
 {
-    // TODO: Will only work for small files. Add more logic for looping if file size is larger
+    // TODO: Buffer overflow fix
+    scanf("%s", location);
+    if (strlen(location) != 3)
+    {
+        printf("\nERR: Locations must have 3 characters\n");
+        exit(1);
+    }
+    to_upper_case_string(location);
+    if (!location_exists(location))
+    {
+        printf("\nLocation does not exist\n");
+        exit(1);
+    }
+    location[3] = '\0';
+}
+
+void create_flight(char *origin, char *destination)
+{
+    char write_buffer[12] = "";
+    FILE *fp = fopen("flights.txt", "a");
+    if (fp == NULL)
+    {
+        printf("\nCould not open dest file to add location\n");
+        exit(2);
+    }
+
+    char filler[] = " => ";
+    strcat(write_buffer, origin);
+    strcat(write_buffer, filler);
+    strcat(write_buffer, destination);
+    strcat(write_buffer, "\n");
+    fwrite(write_buffer, 1, sizeof(write_buffer), fp);
+    fclose(fp);
+    printf("\nFlight created\n");
+}
+
+void add_flight()
+{
+    char origin_location[4];
+    char destination_location[4];
+    select_location(origin_location);
+    select_location(destination_location);
+    create_flight(origin_location, destination_location);
+}
+
+void list_locations()
+{
     char read_buffer[1024], existing_location[4];
     FILE *fdest = fopen("dest.txt", "r");
     fread(read_buffer, 1, 1024, fdest);
@@ -66,7 +115,33 @@ int location_exists(char *location)
         if (read_buffer[i] == '\n')
         {
             existing_location[3] = '\0';
+            printf("%d: %3s\n", (i + 1) / 4, existing_location);
             j = 0;
+        }
+        else
+        {
+            j++;
+        }
+    }
+}
+
+int location_exists(char *location)
+{
+    // TODO: Will only work for small file with less than 1024 bytes of length. Add more logic for looping if file size is larger to read bigger buffer, in chunks
+    char read_buffer[1024], existing_location[4];
+    FILE *fdest = fopen("dest.txt", "r");
+    if (fdest == NULL)
+    {
+        printf("\nCould not open dest file to check existing location\n");
+        exit(2);
+    }
+    fread(read_buffer, 1, 1024, fdest);
+    for (int i = 0, j = 0; read_buffer[i] != EOF; i++)
+    {
+        existing_location[j] = read_buffer[i];
+        if (read_buffer[i] == '\n')
+        {
+            existing_location[3] = '\0';
             if (!strcmp(location, existing_location))
             {
                 return TRUE;
@@ -86,7 +161,7 @@ void add_location_flow()
     char t[4];
     // TODO: Buffer overflow fix
     scanf("%s", t);
-
+    t[3] = '\0';
     if (strlen(t) != 3)
     {
         printf("\nERR: Locations must have 3 characters\n");
